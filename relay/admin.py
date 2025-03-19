48a0c79ec7c81496fb7452d3049a3048f7a12970
NANA KWAME YIADOM BOACHIE <160287824+nana-boachie@users.noreply.github.com>
2025-03-19 12:13:13 +0000

    move to indepent apps completed
    Moving substation equipment and relays into individual apps

--- relay/admin.py
+++ relay/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Relay
 
 # Register your models here.
+
+
+admin.site.register(Relay)
--- substation_equipment/admin.py
+++ substation_equipment/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Transformer, TransmissionLine
 # Register your models here.
+
+
+admin.site.register(Transformer)
+admin.site.register(TransmissionLine)
